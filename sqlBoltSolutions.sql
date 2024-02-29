b7c276baa81c80fac0bc5de435287291e152a4f2
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-02-29 15:03:25 +0300

    feat: add Example 14, Updating rows

--- sqlBoltSolutions.sql
+++ sqlBoltSolutions.sql
@@ -232,5 +232,19 @@ INSERT INTO movies VALUES (4, "Toy Story 4", "El Directore", 2015, 90);
 INSERT INTO boxoffice VALUES (4, 8.7, 340000000, 270000000);
 
 
+                                            -- EXERCİSE 14 --
 
+-- The director for A Bug's Life is incorrect, it was actually directed by John Lasseter
+UPDATE movies
+SET director = "John Lasseter"
+WHERE id = 2
 
+-- The year that Toy Story 2 was released is incorrect, it was actually released in 1999
+UPDATE movies
+SET year = 1999
+WHERE id = 3;
+
+-- Both the title and director for Toy Story 8 is incorrect! The title should be "Toy Story 3" and it was directed by Lee Unkrich
+UPDATE movie
+SET title = "Toy Story 3", director = "Lee Unkrich"
+Where id = 11;
