7cc3f7fcdcebed1558493d8fa79374eeed528d62
AbdullahKarakoc <abdullahkrkc1453@gmail.com>
2024-02-29 15:21:22 +0300

    feat: add Example 18, Dropping tables

--- sqlBoltSolutions.sql
+++ sqlBoltSolutions.sql
@@ -288,4 +288,10 @@ ALTER TABLE movies
 
                                             -- EXERCİSE 18 --
 
+-- We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+DROP TABLE movies;
+
+-- And drop the BoxOffice table as well
+DROP TABLE BoxOffice;
+
 
